7ec730ea7ca000b7b9b86aeb73c56ef17b75647d
Azat <azat.farmanyan2@mail.ru>
2023-04-10 10:15:07 +0400

    feat: uptade backend

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const jwt = require('jsonwebtoken');
-var cors = require('cors')
+const express = require("express");
+const app = express();
+const port = 3000;
+const jwt = require("jsonwebtoken");
+// app.get("/candy", (req, res) => {
+//   res.set("Access-Control-Allow-Origin", "http://localhost:8000");
+//   res.json({ candy: "bubble-gum" });
+// });
+var cors = require("cors");
 app.use(express.json());
-app.use(cors())
-const user ={
-    email:"azat@mail.ru",
-    password:"1234"
-}
-app.post('/login',async (req, res) => {
-  const {email,password} =req.body;
-  console.log(email,password,user[email], email)
-  if(!email.length || user.email != email  ){
-        return res.status(400).json({message:"email is incorrect"})
+app.use(cors());
+const user = {
+  email: "azat@mail.ru",
+  password: "1234",
+};
+app.post("/login", async (req, res) => {
+  // console.log(req);
+  const { email, password } = req.body;
+  // console.log(email, password, user[email], email);
+  if (!email.length || user.email != email) {
+    return res.status(400).json({ message: "email is incorrect" });
   }
-  if(!password?.length || user.password !== password  ){
-        return res.status(400).json({message:"password is incorrect"})
+  if (!password?.length || user.password !== password) {
+    return res.status(400).json({ message: "password is incorrect" });
   }
 
-  const token= await jwt.sign({email},"very secret jwt code")
-  return res.status(200).json({token})
-})
-app.post('/incremend',async (req, res) => {
-    const Auth=req.headers.authorization?.split("Bearer")[1]
-    const {counter}=req.body
-   if(Auth){
-        const result= counter === 0 ? 1 : counter *2;
-        return res.status(200).json(result)
-   }
-   return res.status(401).json({message:"you are not authorized"})
-})
+  const token = await jwt.sign({ email }, "very secret jwt code");
+  return res.status(200).json({ token });
+});
 
+app.post("/increment", async (req, res) => {
+  const Auth = req.headers.authorization?.split("Bearer")[1];
+  const { count } = req.body;
+  if (Auth) {
+    const result = count === 0 ? 1 : count * 2;
+    console.log(result);
+    return res.status(200).json(result);
+  }
+  return res.status(401).json({ message: "you are not authorized" });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
